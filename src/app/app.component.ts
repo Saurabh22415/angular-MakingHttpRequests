402615354604fbce2a74d2fd2ed73cb852158b72
Saurabh22415 <saurabh.ranjan50@gmail.com>
2019-12-15 23:10:19 +0530

    1 file modified, 1 added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {map} from 'rxjs/operators'
+import {Post} from './post.model'
 
 @Component({
   selector: 'app-root',
@@ -16,7 +17,7 @@ export class AppComponent implements OnInit {
     this.fetchPosts();
   }
 
-  onCreatePost(postData: { title: string; content: string }) {
+  onCreatePost(postData: Post) {
     // Send Http request
     this.http
       .post(
@@ -38,8 +39,8 @@ export class AppComponent implements OnInit {
 
   private fetchPosts(){
     this.http.get('https://ng-complete-guide-80c93.firebaseio.com/posts.json').pipe(
-      map(responseData => {
-        const postArray = [];
+      map((responseData : {[key: string]: Post}) => {
+        const postArray :Post[] = [];
           for(const key in responseData)
           {
             if(responseData.hasOwnProperty(key)){
