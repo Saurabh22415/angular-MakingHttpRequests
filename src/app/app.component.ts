afed32bf67ac48ed39b69184ed0dbdba75aa3e58
Saurabh22415 <saurabh.ranjan50@gmail.com>
2019-12-16 06:19:27 -0500

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {map} from 'rxjs/operators'
 import {Post} from './post.model'
+import {PostService} from './post.service';
 
 @Component({
   selector: 'app-root',
@@ -12,22 +13,14 @@ export class AppComponent implements OnInit {
   loadedPosts : Post[] = [];
   isFetching : boolean = false;
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private postService : PostService) {}
 
   ngOnInit() {
     this.fetchPosts();
   }
 
   onCreatePost(postData: Post) {
-    // Send Http request
-    this.http
-      .post(
-        'https://ng-complete-guide-80c93.firebaseio.com/posts.json',
-        postData
-      )
-      .subscribe(responseData => {
-        console.log(responseData);
-      });
+    this.postService.CreateAndSavePost(postData.title, postData.content);
   }
 
   onFetchPosts() {
--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -1,7 +1,23 @@
 import {Injectable} from '@angular/core'
+import {HttpClient} from '@angular/common/http'
+import {Post} from './post.model'
 
 @Injectable({ providedIn : 'root'})
 export class PostService
 {
+  constructor (private http : HttpClient){}
+
+  public CreateAndSavePost(title : string, content : string)
+  {
+    const postData : Post  = {title : title, content : content};
+    this.http
+      .post(
+        'https://ng-complete-guide-80c93.firebaseio.com/posts.json',
+        postData
+      )
+      .subscribe(responseData => {
+        console.log(responseData);
+      });
+  }
 
 }
