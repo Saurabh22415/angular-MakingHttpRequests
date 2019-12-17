afac0b7989572feb360749833cf65bfc47b64f38
Saurabh22415 <saurabh.ranjan50@gmail.com>
2019-12-17 05:51:10 -0500

    2 files modified

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -4,7 +4,9 @@ export class AuthInterceptorService implements HttpInterceptor{
   intercept(req : HttpRequest<any>, next: HttpHandler)
   {
     console.log("request is on way");
-    return next.handle(req);
+    const modifiedReq = req.clone({headers : req.headers.append('auth', 'xyz')});
+    console.log("modified request");
+    return next.handle(modifiedReq);
   }
 
 }
